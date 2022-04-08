8f8435da16e5ffc0736fa515a4e4da6e39eb0875
krxhna <m.krishnakanth1999@gmail.com>
2022-04-08 18:16:38 +0800

    trying and failing three js

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="d">
+    <Renderer resize="window" orbit-ctrl>
+      <Camera :position="{ z: 10 }" />
+      <Scene background="red">
+        <PointLight :position="{ y: 50, z: 50 }" />
+        <Box>
+          <LambertMaterial />
+        </Box>
+      </Scene>
+    </Renderer>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script lang="ts">
+import { Box, Camera, LambertMaterial, PointLight, Renderer, Scene } from "troisjs";
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  components: { Box, Camera, LambertMaterial, PointLight, Renderer, Scene },
+
+  setup() {
+    const renderer = ref(null);
+    const box = ref(null);
+
+    renderer?.value?.onBeforeRender(() => {
+      box.value.mesh.rotation.x += 0.01;
+    });
+
+
+    return{renderer,box};
+  },
+});
+</script>
